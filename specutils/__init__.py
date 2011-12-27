c5a0069b29aa9e82246afd76db9224cbb566ae74
Erik Tollerud <erik.tollerud@gmail.com>
2011-12-27 17:39:07 -0600

    Squash all package-template commit messages
    updated docs Makefile to reflect astropy
    
    Move the py.test conftest.py file to the astropy directory so it is installed. Changed the import of plugins accordingly. Also updated the .gitignore file so it ignores the Mac OS X .DS_Store file.
    
    fixed conf.py to not insert source dir into sys.path
    
    typo fixes in conf.py comment
    
    Added CSS file from Astropy core package
    
    Updated setup.cfg to match Astropy core package
    
    Copy bootstrap-astropy theme from astropy
    
    remove default.css import from astropy.css - now not needed
    
    update conf.py to use bootstrap-astropy theme, mirror astropy's version
    
    use installed astropy theme
    
    Added templates that inherit from core package
    
    Added `_templates` to list of patterns to exclude
    
    added comment directing to base of jinja template
    
    Update testing to only import pytest when tests are run
    
    Added `coverage` option that was added in the Astropy core
    
    Removed comments
    
    added kwarg options to test runner
    
    removed line setting unused _test_runner variable
    
    Updated setup.py to bring it in line with the Astropy core
    
    Update distribute_setup.py to 0.6.28
    
    Remove unnecessary instructions from README.rst
    
    tweak to instructions from @astrofrog's suggestion
    
    Changed check in setup.py to syntax in core package to avoid issues when Sphinx is not installed.
    
    Update setup.py to reflect re-factored setup code for 0.2 release
    
    If necessary, move configuration file to the right place on first import
    
    Added open_files option for test() and implemented fix for caught exception in __init__.py
    
    adjusted setup.py for specutils details
    
    had to rename the packagename stuff to specutils

--- specutils/__init__.py
+++ specutils/__init__.py
@@ -20,6 +20,18 @@ except NameError:
     builtins._PACKAGE_SETUP_ = False
     del version_info
 
+#this indicates whether or not we are in the package's setup.py
+try:
+    _PACKAGE_SETUP_
+except NameError:
+    from sys import version_info
+    if version_info[0] >= 3:
+        import builtins
+    else:
+        import __builtin__ as builtins
+    builtins._PACKAGE_SETUP_ = False
+    del version_info
+
 try:
     from .version import version as __version__
 except ImportError:
